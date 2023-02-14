87bba5587fbace042d0780de49a99ddc3ac618ee
Max Kozlov <68003929+M4x1mch1k@users.noreply.github.com>
2023-02-14 22:02:45 +0200

    first tab

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,20 @@ export class Tab1Page {
 
   constructor() {}
 
+  a!: number;
+  b!: number;
+  c!: number;
+  d!: number;
+
+  ras (a: string, b: string, c: string) {
+    this.a = parseFloat(a!);
+    this.b = parseFloat(b!);
+    this.c = parseFloat(c!);
+
+    if ((this.a % 2 == 0) || (this.b % 2 == 0) || (this.c % 2 == 0))
+      this.d = Math.pow((this.a + this.b + this.c), 3)
+    else
+      this.d = Math.pow(this.a, 3) + Math.pow(this.b, 3) + Math.pow(this.c, 3)
+  }
+
 }
